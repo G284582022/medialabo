2fe4cb0387564716bdd4b3d7cb6abde934fd368f
G284582022 <228458@st.takushoku-u.ac.jp>
2023-05-11 16:51:35 +0900

    e

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -20,7 +20,7 @@ function hantei() {
   let yoso = 4;
   kaisu += 1;
   console.log(kaisu + "回目の予想: " + yoso)
-  if (kaisu > 4) {
+  if (kaisu > 3) {
     console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
   }else{
     if (yoso > kotae) {
