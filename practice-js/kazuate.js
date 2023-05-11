c402c20c5fcbe5a9088710089dfc4b9df0751b32
G284582022 <228458@st.takushoku-u.ac.jp>
2023-05-11 15:57:40 +0900

    数当て

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -18,8 +18,18 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  kaisu += 1;
+  console.log(kaisu + "回目の予想: " + yoso)
+  if (kaisu > 4) {
+    console.log("答えは" + kotae +"でした．すでにゲームは終わっています");
+  }else{
+    if (yoso > kotae) {
+        console.log("答えはもっと小さいですよ");
+    }else if(yoso < kotae) {
+        console.log("答えはもっと大きいですよ");
+    }elseconsole.log("正解です.おめでとう!");
+    }
+}
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-}
