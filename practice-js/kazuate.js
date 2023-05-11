da29aa9d322a38688c6f3a20433534b5ced90553
G284582022 <228458@st.takushoku-u.ac.jp>
2023-05-11 16:01:13 +0900

    改

--- practice-js/kazuate.js
+++ practice-js/kazuate.js
@@ -15,7 +15,7 @@ hantei();
 hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() 
+function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
   kaisu += 1;
@@ -30,6 +30,7 @@ function hantei()
     }else{
         console.log("正解です.おめでとう!");
     }
+  }
 }
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
