09ab9eeece914f8fae3831c14e69c59901fecfc7
G284582022 <228458@st.takushoku-u.ac.jp>
2023-06-01 16:42:40 +0900

    sss

--- weather.js
+++ weather.js
@@ -49,19 +49,43 @@
 
 */
 
-let b = document.querySelector('#sendRequest');
-b.addEventListener('click', sendRequest);
-  let i = document.querySelector('imput[name ="yoso"]')
+let b1 = document.querySelector('button#print');
+b1.addEventListener('click',kion );
+function kion() {
+  let i = document.querySelector('input[name="yoso"]');
+  let g=i.value;
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+g+'.json';
   axios.get(url)
-        .then(showResult)   // 通信成功
-        .catch(showError)   // 通信失敗
-        .then(finish);      // 通信の最後の処理
+  .then(showResult)   // 通信成功
+  .catch(showError)   // 通信失敗
+  .then(finish);      // 通信の最後の処理
+}
 
-function showResult(resp) { 
+function showResult(resp){
   let data = resp.data;
   if (typeof data === 'string') {
     data = JSON.parse(data);
-    console.log(data);
-    console.log(data.x);
   }
+  console.log(data);
+  console.log(data.name);
+  console.log(data.main.temp_max);
+  console.log(data.main.temp_min);
+  let p1 = document.querySelector('p#name');
+p1.textContent = ("都市名: "+data.name); 
+let p2 = document.querySelector('p#temp_max');
+p2.textContent = ("最高気温: "+data.main.temp_max+"℃");
+let p3 = document.querySelector('p#temp_min');
+p3.textContent = ("最低気温: "+data.main.temp_min+"℃");
+
 }
+
+function showError(err) {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
+
+
+
+
